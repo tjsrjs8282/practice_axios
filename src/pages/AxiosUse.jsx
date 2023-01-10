a32c4784d6e8eab7ddd8c27a82f28edd52ff20f5
seongeon <dbtjsrjs8282@naver.com>
2023-01-11 01:45:13 +0900

    axios instance interceptors

--- src/pages/AxiosUse.jsx
+++ src/pages/AxiosUse.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react";
+import axiosInstance from "../api/todoAxios";
 import axios from "axios";
 
 const AxiosUse = () => {
   const [todoTitle, setTodoTitle] = useState([]);
-  function todoApi() {
-    const url = "https://jsonplaceholder.typicode.com/todos/";
 
-    axios
-      .get(url)
-      .then((res) => setTodoTitle(res.data))
-      .catch((error) => {
-        console.log("데이터를 불러오지 못했습니다");
-      });
+  // function todoApi() {
+  //   const url = "https://jsonplaceholder.typicode.com/todos/";
+
+  //   axios
+  //     .get(url)
+  //     .then((res) => setTodoTitle(res.data))
+  //     .catch((error) => {
+  //       console.log("데이터를 불러오지 못했습니다");
+  //     });
+  // }
+  function todoApi() {
+    axiosInstance
+      .get()
+      .then((res) => setTodoTitle(res))
+      .catch((err) => console.log(err));
   }
 
   useEffect(() => {
